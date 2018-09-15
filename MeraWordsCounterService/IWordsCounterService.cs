6777867048dd8bb14b0e87087fef1847599a77cd
PredYello <jelovaczr@yahoo.com>
2018-09-15 18:31:58 +0200

    MeraWordsCounterService added (again)

---
+++ MeraWordsCounterService/IWordsCounterService.cs
@@ -0,0 +1,18 @@
+﻿using MeraWordsCounter.Model;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace MeraWordsCounterService
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+	[ServiceContract]
+	public interface IWordsCounterService
+	{
+
+		[OperationContract]
+		int GetWordCount(int textId);
+
+		[OperationContract]
+		List<Text> GetAvailableTexts();
+	}
+}
---
+++ MeraWordsCounterService/WordsCounterService.svc.cs
@@ -0,0 +1,29 @@
+﻿using MeraWordsCounter.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MeraWordsCounterService
+{
+	public class WordsCounterService : IWordsCounterService
+	{
+		public WordsCounterService()
+		{
+			
+		}
+
+		public int GetWordCount(int textId)
+		{
+			return 5;
+		}
+
+		public List<Text> GetAvailableTexts()
+		{
+			return new List<Text>() { new Text() { Id = 1 }, new Text() { Id = 2 } };
+		}
+	}
+}
